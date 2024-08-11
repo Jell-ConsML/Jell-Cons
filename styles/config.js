ac7cb6a6223c99bfdebb2daec8fcf87cb1adfbc0
Jellcons <sapphireflowers1721@gmail.com>
2024-08-11 00:16:11 -0400

    send
    send

--- styles/config.js
+++ styles/config.js
@@ -2,7 +2,7 @@
 /* Sheet ID
 /* Your sheet ID
 /* ------------------------------------------------------------------- */
-let sheetID = "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak";
+let sheetID = "1jZXSiBMRT_LOcZbdcHz9NEdgif6YBuJOg3JO0Kl1gQg";
 
 
 /* ------------------------------------------------------------------- */
